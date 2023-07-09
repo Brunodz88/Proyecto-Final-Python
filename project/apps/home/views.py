ad715471dbfd5b4fda5a2666ec9d5d6e195333e5
Bruno Diaz <brunoodiaz.88@gmail.com>
2023-07-09 16:23:54 -0300

    commit ahora

--- project/apps/home/views.py
+++ project/apps/home/views.py
@@ -65,13 +65,3 @@ def about(request: HttpRequest) -> HttpResponse:
     return render(request, "home/about.html")
 
 
-# @staff_member_required
-# def register(request: HttpRequest) -> HttpResponse:
-#     if request.method == "POST":
-#         form = forms.CustomUserCreationForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return render(request, "home/index.html", {"messages": "Vendedor creado 👌"})
-#     else:
-#         form = forms.CustomUserCreationForm()
-#     return render(request, "home/register.html", {"form": form})
