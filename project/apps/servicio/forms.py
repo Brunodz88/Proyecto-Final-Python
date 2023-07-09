6aae15531b6f8fd7d7a8b70f87e762019a93fb01
Bruno Diaz <brunoodiaz.88@gmail.com>
2023-07-09 20:30:40 -0300

    modificaciones

--- project/apps/servicio/forms.py
+++ project/apps/servicio/forms.py
@@ -7,17 +7,15 @@ from .models import Obra, Comentario
 class FormularioNuevaObra(forms.ModelForm):
     class Meta:
         model = Obra
-        fields = ("usuario", "titulo", "obra", "marca", "modelo", "descripcion", "year", "precio", "telefonoContacto", "emailContacto", "imagenObra")
+        fields = ("usuario", "titulo", "obra", "descripcion", "añodeejecución", "valor", "telefonoContacto", "emailContacto", "imagenObra")
 
         widgets = {
             "usuario": forms.TextInput(attrs={"class": "form-control", "value": "", "id": "usuario_id", "type": "hidden"}),
             "titulo": forms.TextInput(attrs={"class": "form-control"}),
             "Obra": forms.Select(attrs={"class": "form-control"}),
-            "marca": forms.TextInput(attrs={"class": "form-control"}),
-            "modelo": forms.TextInput(attrs={"class": "form-control"}),
             "descripcion": forms.Textarea(attrs={"class": "form-control"}),
-            "year": forms.TextInput(attrs={"class": "form-control"}),
-            "precio": forms.TextInput(attrs={"class": "form-control"}),
+            "añodeejecución": forms.TextInput(attrs={"class": "form-control"}),
+            "valor": forms.TextInput(attrs={"class": "form-control"}),
             "telefonoContacto": forms.TextInput(attrs={"class": "form-control"}),
             "emailContacto": forms.TextInput(attrs={"class": "form-control"}),
         }
